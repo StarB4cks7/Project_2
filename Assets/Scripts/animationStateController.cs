4716bfcfdfdf6ff38b98c7b473ba5ad33097b90f
Friedrich <born.daniel11@gmail.com>
2022-11-10 15:48:14 +0100

    Aniamtion Fixes

---
+++ Assets/Scripts/animationStateController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animationStateController : MonoBehaviour
+{
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+  
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool isRunning = animator.GetBool("IsRunning");
+        bool forwardPressed = Input.GetKey("w");
+
+        //If Player presses W runnuing animation plays
+        if (!isRunning && forwardPressed)
+        {
+            animator.SetBool("IsRunning", true);
+        }
+
+        //Resets Boolien to false 
+        if (isRunning && !forwardPressed)
+        {
+            animator.SetBool("IsRunning", false);
+        }
+    }
+}
