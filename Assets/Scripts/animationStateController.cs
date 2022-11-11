fe22321e44e518143b71cead38ec3181f1071761
Friedrich <born.daniel11@gmail.com>
2022-11-11 12:03:11 +0100

    Save Before Jumping animation

--- Assets/Scripts/animationStateController.cs
+++ Assets/Scripts/animationStateController.cs
@@ -6,12 +6,14 @@ public class animationStateController : MonoBehaviour
 {
     Animator animator;
 
+
     private bool isJumbing;
     private bool isGrounded;
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+
   
         
     }
@@ -23,10 +25,18 @@ public class animationStateController : MonoBehaviour
         bool forwardPressed = Input.GetKey("w");
 
         //If Player presses W runnuing animation plays
-        if (!isRunning && forwardPressed)
+        if (!isRunning && forwardPressed && isJumbing==false)
         {
             animator.SetBool("IsRunning", true);
+
+           
         }
+
+        if (isGrounded == false)
+        {
+            animator.SetBool("IsRunning", false);
+        }
+        
 
         //Resets Boolien to false 
         if (isRunning && !forwardPressed)
@@ -34,4 +44,31 @@ public class animationStateController : MonoBehaviour
             animator.SetBool("IsRunning", false);
         }
     }
+
+    private void OnTriggerStay(Collider other)
+    {
+        isGrounded = true;
+        Debug.Log("Groudn check animator");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.tag == "Ground")
+        {
+            isJumbing = false;
+            Debug.Log("landed");
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Ground")
+        {
+            isGrounded = false;
+            isJumbing = true;
+
+        }
+    }
+
+    
 }
